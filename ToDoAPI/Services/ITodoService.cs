99bcfbea26a9a246c0f2645a05140b203a3b6bc9
wiktormuller <biznes.wiktor@gmail.com>
2020-07-29 12:23:29 +0200

    Simple synchornous implementation of service layer

---
+++ ToDoAPI/Services/ITodoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Services
+{
+    public interface ITodoService
+    {
+        IEnumerable<TodoItem> GetAllTodoItems();
+        TodoItem GetTodoItemById(Guid id);
+        void CreateTodoItem(TodoItem todoItem);
+        void UpdateTodoItem(TodoItem todoItem);
+        void DeleteTodoItem(TodoItem todoItem);
+    }
+}
---
+++ ToDoAPI/Services/TodoService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Services
+{
+    public class TodoService : ITodoService
+    {
+        private readonly TodoContext _context;
+
+        public TodoService(TodoContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateTodoItem(TodoItem todoItem)
+        {
+            if(todoItem == null)
+            {
+                throw new ArgumentNullException(nameof(todoItem));
+            }
+            _context.TodoItems.Add(todoItem);
+        }
+
+        public void DeleteTodoItem(TodoItem todoItem)
+        {
+            if(todoItem == null)
+            {
+                throw new ArgumentNullException(nameof(todoItem));
+            }
+
+            _context.TodoItems.Remove(todoItem);
+        }
+
+        public IEnumerable<TodoItem> GetAllTodoItems()
+        {
+            var todoItems = _context.TodoItems.ToList();
+
+            return todoItems;
+        }
+
+        public TodoItem GetTodoItemById(Guid id) //How to make it async?
+        {
+            var todoItem = _context.TodoItems.Find(id);
+
+            return todoItem;
+        }
+
+        public void UpdateTodoItem(TodoItem todoItem)
+        {
+            //Nothing -> EF Core update it's automatically ???
+        }
+    }
+}
